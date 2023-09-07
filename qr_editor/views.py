09482adec5bc06676b2625c6698976f9060ae55e
yodalightsabr <yoda@yodacode.xyz>
2023-09-07 11:38:56 -0700

    Update views.py

--- qr_editor/views.py
+++ qr_editor/views.py
@@ -27,7 +27,7 @@ def get_qr_template(request):
     if 'errorCorrectionLevel' in request.POST:
         error = request.POST['errorCorrectionLevel']
 
-    urlPrefix = 'HTTPS://MY-QR.ART/R'
+    urlPrefix = 'HTTPS://QR.HACK.AF/'
     if 'customUrlPrefix' in request.POST:
         urlPrefix = request.POST['customUrlPrefix'].upper()
     
